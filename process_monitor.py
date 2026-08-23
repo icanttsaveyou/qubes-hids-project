b04e139d447b1320c633a8f70645aaceed4e1ad7
icanttsaveyou <iluvcompsci01@proton.me>
2026-08-22 22:15:49 -0400

    Add process monitoring for suspicious executable paths
    Implement process inspection for suspicious paths.

---
+++ process_monitor.py
@@ -0,0 +1,21 @@
+import psutil
+
+SUSPICIOUS_PATHS = ["/tmp", "/var/tmp", "/dev/shm"]
+
+def inspect_processes():
+    findings = []
+    for proc in psutil.process_inter(['pid', 'name', 'exe']
+        try:
+            exe_path = proc.info['exe']
+            if exe_path:
+                for bad_path in SUSPICIOUS_PATHS:
+                    if exe_path.startswith(bad_path);
+                        findings.append({
+                            "type": "SUSPICIOUS_PROCESS",
+                            "pid": proc.info['pid']
+                            "name": proc.info['name'],
+                            "path": exe_path
+                        })   
+        except 9psutil.NoSuchProcess, psutil.AccessDenied):
+            continue 
+    return findings
